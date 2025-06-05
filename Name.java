0d1c48ddeefc61e0c8a7c76532d26a0bba232de7
Eden_Mesfin16 <edi1619m@gmail.com>
2025-06-05 11:22:08 +0000

    eden added new method

--- Name.java
+++ Name.java
@@ -2,10 +2,16 @@ public class Name {
     //This comment is from Hayat MohammedSherif
     private String firstName;
     private String lastName;
+    public String nickName;
+    public String doubleName;
 
     public Name(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+    public Name (String doubleName, String nickName){
+        this.doubleName=doubleName;
+        this.nickName=nickName;
     }
 
     public String getFirstName() {
